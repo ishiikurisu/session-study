d3b4744e97936308e103352859f9ced06b07d1e8
Cristiano Alves da Silva Junior <cristiano.junior@mbra.com.br>
2018-11-01 15:27:36 -0300

    Learning how to combine actions routes and static files

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
 var express = require('express');
 var app = express();
 
-// TODO use the following line
-// app.use(express.static('public'));
+app.use(express.static('www'));
 
-app.get('/', function (req, res) {
-    var options = {
-        root: __dirname,
-        dotfiles: 'deny',
-        headers: {
-            'x-timestamp': Date.now(),
-            'x-sent': true
-        }
-    };
-
-    res.sendFile("/index.html", options, function (err) {
-        if (err) {
-            next(err);
-        } else {
-            console.log('Sent:', fileName);
-        }
-    });
+app.get('/api/something', function (req, res) {
+    res.send('nothing...');
 });
 
 app.listen(3000, function () {
---
+++ www/js/index.js
@@ -0,0 +1,11 @@
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+function saySomething() {
+    var something = httpGet("/api/something");
+    alert(something);
+}
